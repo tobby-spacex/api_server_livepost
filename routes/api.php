7729072dce3963507ff9fd8ea6837c3bdd8ab436
Arman Fayziev <arman.fayziev@scandiweb.com>
2023-03-30 01:24:44 +0300

    route helper for folder iterating

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Helpers\Routes\RouteHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')
     ->group(function (){
-        require __DIR__ . '/api/v1/users.php';
-        require __DIR__ . '/api/v1/posts.php';
-        require __DIR__ . '/api/v1/comments.php';
+
+        RouteHelper::includeRouteFiles(__DIR__ . '/api/v1');
+        // require __DIR__ . '/api/v1/users.php';
+        // require __DIR__ . '/api/v1/posts.php';
+        // require __DIR__ . '/api/v1/comments.php';
     });
 
 
